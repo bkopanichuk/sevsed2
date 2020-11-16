34658a0e7276d3fc1d7a834f19b342aeabbe9006
letsolexandr <letsolexandr@gmail.com>
2020-11-16 13:37:27 +0200

    fix preview generator

--- apps/document/services/document_service.py
+++ apps/document/services/document_service.py
@@ -1,7 +1,12 @@
 import logging
-
+import os
+import glob
 from ..models.document_model import MainFileVersion
-from ..shared_tasks.tasks import generate_text, create_preview
+from apps.document.shared_tasks.tasks import generate_text
+from preview_generator.manager import PreviewManager
+
+from apps.document.models.document_model import get_upload_document_path, get_preview_directory
+from config.settings import MEDIA_ROOT
 
 logger = logging.getLogger("debug")
 
@@ -18,8 +23,6 @@ class GenerateText:
 
 
 class CreatePreview:
-    """Створити файл для попередного перегляду"""
-
     def __init__(self, doc, update_fields):
         self.document = doc
         self.update_fields = update_fields
@@ -29,8 +32,33 @@ class CreatePreview:
             if list(self.update_fields)[0] in UPDATED_FIELDS:
                 return
                 ##raise Exception(self.update_fields)
-        create_preview.delay(self.document.unique_uuid)
+        self.create_preview()
         self.document.save(update_fields=UPDATED_FIELDS)
+
+    def create_preview(self):
+        upload_document_path = get_upload_document_path(self.document, os.path.basename(self.document.main_file.name))
+        upload_cache_path = get_preview_directory(self.document, os.path.basename(self.document.main_file.name))
+        if self.document.main_file.name.endswith('.pdf'):
+            self.document.preview_pdf.name = upload_document_path
+            return
+        full_path = os.path.join(MEDIA_ROOT, upload_document_path)
+        full_cache_path = os.path.join(MEDIA_ROOT, upload_cache_path)
+        cache_path = os.path.dirname(full_cache_path)
+
+        manager = PreviewManager(cache_path, create_folder=True)
+        path_to_preview_image = manager.get_jpeg_preview(full_path, width=1920, height=1080)
+        preview_dir_path = os.path.dirname(path_to_preview_image)
+        preview_base_name = os.path.basename(path_to_preview_image)
+        self.document.preview.name = os.path.join(upload_cache_path, os.path.basename(path_to_preview_image))
+
+        if self.document.main_file.name.endswith('.pdf'):
+            self.document.preview_pdf.name = upload_document_path
+            return
+
+        os.chdir(preview_dir_path)
+        for file in glob.glob(preview_base_name.replace('-1920x1080.jpeg', '') + "*.pdf"):
+            self.document.preview_pdf.name = os.path.join(upload_cache_path, os.path.basename(file))
+            return
 
 
 class UpdateMainFileVersion:
